edd781838e85bb91079073a981d0aad3e201afe5
alexknorr <alexander.knorr416@gmail.com>
2021-12-01 08:22:47 -0800

    completed chapter 2

--- Chptr2_Dist/Chptr2_dist.py
+++ Chptr2_Dist/Chptr2_dist.py
@@ -2,6 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.utils import resample
+from scipy import stats
+import numpy as np
 
 loans_income = pd.read_csv("/Users/alexknorr/PycharmProjects/Practical-Stats-for-DS/data/loans_income.csv", squeeze=True)
 
@@ -34,6 +36,26 @@ print("Bootstrap Statistics:")
 print(f"original: {loans_income.median()}")
 print(f"bias:{results.mean() - loans_income.median()}")
 print(f"std. error:{results.std()}")
+
+fig, ax = plt.subplots(figsize=(4, 4))
+norm_sample = stats.norm.rvs(size=100)
+stats.probplot(norm_sample, plot=ax)
+
+sp500_px = pd.read_csv("/Users/alexknorr/PycharmProjects/Practical-Stats-for-DS/data/sp500_data.csv.gz")
+nflx = sp500_px.NFLX
+nflx = np.diff(np.log(nflx[nflx > 0]))
+fig, ax = plt.subplots(figsize=(4, 4))
+stats.probplot(nflx, plot=ax)
+
+print(stats.binom.pmf(2, n=5, p=0.1))
+print(stats.binom.cdf(2, n=5, p=0.1))
+
+print(stats.poisson.rvs(2, size=100))
+
+print(stats.expon.rvs(0.2, size=100))
+
+print(stats.weibull_min.rvs(1.5, scale=5000, size=100))
+
 
 
 
